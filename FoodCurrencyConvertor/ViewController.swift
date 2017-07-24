4d63c099cf2c40b0cbff27724f86758bc0579ace
priya <priya.xavier@gmail.com>
2017-07-24 17:51:09 -0500

    Add currency conversion picker view

--- FoodCurrencyConvertor/ViewController.swift
+++ FoodCurrencyConvertor/ViewController.swift
@@ -10,9 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  // Action buttons
-  @IBAction func checkoutBtn(_ sender: UIButton) {
-  }
+ 
   
   //Outlet labels
   @IBOutlet weak var currencyPicker: UIPickerView!
@@ -29,11 +27,16 @@ class ViewController: UIViewController {
   
   var checkoutTotalUSD: Float = 0.0
   var currencyMultiplier: Float = 1.0
+  var currencyData : [String] = ["1.0", "1.2", "1.3", "1.4","1.5"]
+  var numberFormatter = NumberFormatter()
   
   var groceryList: [GroceryItem] = [GroceryItem(name:"Peas: $ 0,95 per bag",price: 0.95, qty: 0),GroceryItem(name:"Eggs: $ 2,10 per dozen",price: 2.10, qty: 0),GroceryItem(name:"Milk: $ 1,30 per bottle",price: 1.30, qty: 0),GroceryItem(name:"Beans: $ 0,73 per can",price: 0.73, qty: 0)]
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    tableView.delegate = self
+    currencyPicker.delegate = self
+    loadCurrencyData()
    
   }
 
@@ -42,6 +45,19 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  // Action buttons
+  @IBAction func checkoutBtn(_ sender: UIButton) {
+    self.totalLabel.text = numberFormatter.string(from: NSNumber(value: checkoutTotalUSD * currencyMultiplier)) //String(checkoutTotalUSD * currencyMultiplier)
+    
+  }
+  
+  //gets updated currency list
+  func loadCurrencyData() {
+    
+    //make API call
+    self.currencyData = ["1.0", "1.2", "1.3", "1.4","1.5"]
+    currencyPicker.reloadAllComponents()
+  }
 
 }
 
@@ -55,7 +71,7 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate, GroceryCel
     let row = (indexPath as NSIndexPath).row
     let cell = tableView.dequeueReusableCell(withIdentifier: "groceryCell", for: indexPath) as! GroceryCell
     cell.nameLabel.text = groceryList[row].name
-    cell.qtyLabel.text = String(groceryList[row].qty)
+    cell.qtyLabel.text = numberFormatter.string(from: NSNumber(value: groceryList[row].qty)) //String(groceryList[row].qty)
     cell.groceryCellDelegate = self
     cell.tag = row
     return cell
@@ -78,5 +94,25 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate, GroceryCel
        print("I have removed an item with a price tag \(self.groceryList[tag].price)")
       tableView.reloadData()
     }
+  }
+}
+
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return currencyData.count
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    return currencyData[row]
+  }
+ 
+  func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    currencyMultiplier = numberFormatter.number(from: currencyData[row])!.floatValue
+    print(currencyMultiplier)
   }
 }
